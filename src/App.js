2ed2ddff9b85f0563cf5a72b7ff9404104e50837
AlexShin <alex.shin@yale.edu>
2023-03-29 17:43:35 -0400

    updated css for mobile compatibility

--- src/App.js
+++ src/App.js
@@ -52,21 +52,23 @@ function Board({xIsNext, squares, onPlay}) {
   }
   return (
     <>
-      <div className='status'>{status}</div>
-      <div className='board-row'>
-        <Square value = {squares[0]} onSquareClick = {() => handleClick(0)}/> 
-        <Square value = {squares[1]} onSquareClick = {() => handleClick(1)}/>
-        <Square value = {squares[2]} onSquareClick = {() => handleClick(2)}/>
-      </div>
-      <div className='board-row'>
-        <Square value = {squares[3]} onSquareClick = {() => handleClick(3)}/>
-        <Square value = {squares[4]} onSquareClick = {() => handleClick(4)}/>
-        <Square value = {squares[5]} onSquareClick = {() => handleClick(5)}/>
-      </div>
-      <div className='board-row'>
-        <Square value = {squares[6]} onSquareClick = {() => handleClick(6)}/>
-        <Square value = {squares[7]} onSquareClick = {() => handleClick(7)}/>
-        <Square value = {squares[8]} onSquareClick = {() => handleClick(8)}/>
+      <div>
+        <div className='status'>{status}</div>
+        <div className='board-row'>
+          <Square value = {squares[0]} onSquareClick = {() => handleClick(0)}/> 
+          <Square value = {squares[1]} onSquareClick = {() => handleClick(1)}/>
+          <Square value = {squares[2]} onSquareClick = {() => handleClick(2)}/>
+        </div>
+        <div className='board-row'>
+          <Square value = {squares[3]} onSquareClick = {() => handleClick(3)}/>
+          <Square value = {squares[4]} onSquareClick = {() => handleClick(4)}/>
+          <Square value = {squares[5]} onSquareClick = {() => handleClick(5)}/>
+        </div>
+        <div className='board-row'>
+          <Square value = {squares[6]} onSquareClick = {() => handleClick(6)}/>
+          <Square value = {squares[7]} onSquareClick = {() => handleClick(7)}/>
+          <Square value = {squares[8]} onSquareClick = {() => handleClick(8)}/>
+        </div>
       </div>
     </>
   ); 
@@ -104,8 +106,10 @@ function Game(){
 
   return (
     <>
-      <h1 className='game-title'>Shin's TicTacToe Game</h1>
       <div className='game'>
+        <div className = 'top-title'>
+          <h1 className='game-title'>Shin's TicTacToe Game</h1>
+        </div>
         <div className='game-board'>
           <Board xIsNext = {xIsNext} squares = {currentSquares} onPlay = {handlePlay}/>
         </div>
